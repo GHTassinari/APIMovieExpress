e3c917527bb9d729918c338567fd469dc1ced619
guilherme <guilhermehen.tassinari@gmail.com>
2024-09-10 11:58:01 -0300

    Creating update and delete methods for the UserController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,7 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
+const knex = require("../database/knex");
 
 class UsersController {
   async create(req, res){
@@ -18,6 +19,60 @@ class UsersController {
     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
 
     return res.status(201).json();
+  }
+
+  async update(req,res){
+    const { name, email, password, old_password } = req.body;
+    const { id } = req.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user){
+      throw new AppError("User was not found!");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("This e-mail is already being used");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password){
+      throw new AppError("You need to inform the old password in order to create a new password");
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword){
+        throw new AppError("The old password informed, ins't equal to the account's password");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password =?,
+      updated_at = DATETIME('now')
+      WHERE id = ?
+      `, [user.name, user.email, user.password, id]);
+
+      return res.status(200).json();
+  }
+
+  async delete(req,res){
+    const { id } = req.params;
+
+    await knex("users").where({ id }).delete();
+
+    return res.json();
   }
 }
 
