19bc31f7fdfaeb82703094dd4233627cf3ae8c2d
guilherme <guilhermehen.tassinari@gmail.com>
2024-09-12 00:47:24 -0300

    Created a post method to the movie notes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -3,7 +3,27 @@ const knex = require("../database/knex");
 
 class MovieNotesController {
   async create(req, res) {
-    
+    const { title, description, rating, tags } = req.body;
+    const { user_id } = req.params;
+
+    const [movie_note_id] = await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id,
+    });
+
+    const movieTagsInsert = tags.split(',').map((name) => {
+      return {
+        movie_note_id,
+        name,
+        user_id,
+      };
+    });
+
+    await knex("movie_tags").insert(movieTagsInsert);
+
+    res.json();
   }
 }
 
