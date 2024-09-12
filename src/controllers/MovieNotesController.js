f7b88533b475f208ec1486288837642241399781
guilherme <guilhermehen.tassinari@gmail.com>
2024-09-12 01:29:03 -0300

    Creating methods for movie notes and movie tags, inside their respective controllers. The API is finished

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -25,6 +25,65 @@ class MovieNotesController {
 
     res.json();
   }
+  
+  async show(req, res){
+    const { id } = req.params;
+
+    const movie_notes = await knex("movie_notes").where({ id }).first();
+    const movie_tags = await knex("movie_tags").where({ movie_note_id: id }).orderBy("name");
+
+    return res.json({
+      ...movie_notes,
+      movie_tags,
+    });
+  }
+
+  async delete(req, res){
+    const { id } = req.params;
+
+    await knex("movie_notes").where({ id }).delete();
+
+    return res.json();
+  }
+
+  async index(req,res){
+    const { title, user_id, tags } = req.query;
+
+    let movieNotesTags;
+
+    if(tags){
+      const filterMovieTags = tags.split(",").map((tag) => tag.trim());
+
+      movieNotesTags = await knex("movie_tags")
+      .select([
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.user_id",
+      ])
+      .where("movie_notes.user_id", user_id)
+      .whereLike("movie_notes.title", `%${title}%`)
+      .whereIn("movie_tags.name", filterMovieTags)
+      .innerJoin("movie_notes", "movie_notes.id", "movie_tags.movie_note_id")
+      .orderBy("movie_notes.title");
+    } else {
+      movieNotesTags = await knex("movie_notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
+
+    const userTags = await knex("movie_tags").where({ user_id });
+    const movieNotesWithTags = movieNotesTags.map(movieNote => {
+      const movieNotesTags = userTags.filter(userTag => userTag.movie_note_id === movieNote.id);
+      
+      return{
+        ...movieNote,
+        tags: movieNotesTags
+      }
+    })
+
+    return res.json(movieNotesWithTags);
+  }
 }
 
 module.exports = MovieNotesController;
--- src/controllers/MovieTagsController.js
+++ src/controllers/MovieTagsController.js
@@ -2,5 +2,13 @@ const knex = require("../database/knex");
 
 class MovieTagsController {
   async index(req, res){
+    const { user_id } = req.params;
+
+    const movieTags = await knex("movie_tags")
+    .where({ user_id })
+
+    return res.json(movieTags);
   }
 }
+
+module.exports = MovieTagsController;
