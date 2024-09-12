ed0ba4fef12d58a72743eb45437097174716e350
guilherme <guilhermehen.tassinari@gmail.com>
2024-09-12 00:28:02 -0300

    Created movie tags controller and movie tags routes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -2,7 +2,9 @@
 const knex = require("../database/knex");
 
 class MovieNotesController {
-
+  async create(req, res) {
+    
+  }
 }
 
 module.exports = MovieNotesController;
---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,6 @@
+const knex = require("../database/knex");
+
+class MovieTagsController {
+  async index(req, res){
+  }
+}
